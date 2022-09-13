909c72a66987fa107f552e263570ceb03527a337
kakarlasrija <srijakakarla8@gmail.com>
2022-09-13 17:02:06 +0530

    added the project code

---
+++ src/main/java/com/sri/jenkin_project/greetingController.java
@@ -0,0 +1,14 @@
+package com.sri.jenkin_project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class greetingController {
+    
+    @RequestMapping("/Hey")
+    public String name() {
+        System.out.println("let's say hi to jenkins");
+        return "jenkins build maven project successfully";
+    }
+}
